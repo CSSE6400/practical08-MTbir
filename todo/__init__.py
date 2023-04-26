367bb2a58b3256a1e9dcacd48b760ddc55cf8515
MTbir <88026277+MTbir@users.noreply.github.com>
2023-04-26 00:51:46 +0000

    Done week 9 prac

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,8 +1,23 @@
 import os
 import boto3
 import watchtower, logging
-from flask import Flask
+from flask import Flask, has_request_context, request
 from flask_sqlalchemy import SQLAlchemy 
+import uuid
+
+
+class StructuredFormatter(watchtower.CloudWatchLogFormatter):
+   def format(self, record):
+      record.msg = {
+         'timestamp': record.created,
+         'location': record.name,
+         'message': record.msg,
+      }
+      if has_request_context():
+         record.msg['request_id'] = request.environ.get('REQUEST_ID')
+         record.msg['url'] = request.environ.get('PATH_INFO')
+         record.msg['method'] = request.environ.get('REQUEST_METHOD')
+      return super().format(record)
 
 def create_app(config_overrides=None): 
    logging.basicConfig(level=logging.INFO)
@@ -17,7 +32,18 @@ def create_app(config_overrides=None):
            log_group_name="taskoverflow",
            boto3_client=boto3.client("logs", region_name="us-east-1")
    )
+   handler.setFormatter(StructuredFormatter())
    app.logger.addHandler(handler)
+
+   @app.before_request
+   def before_request():
+      request.environ['REQUEST_ID'] = str(uuid.uuid4())
+      requests.info("Request started")
+   @app.after_request
+   def after_request(response):
+      requests.info("Request finished")
+      return response
+
    logging.getLogger().addHandler(handler)
    logging.getLogger('werkzeug').addHandler(handler)
    logging.getLogger("sqlalchemy.engine").addHandler(handler)
